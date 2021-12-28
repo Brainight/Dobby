d763e4ec9909288cd9efa6af2d0d994564be325b
Brainight <lordstoneddog@gmail.com>
2021-12-29 00:19:40 +0100

    Adding commandline options

--- dobby.py
+++ dobby.py
@@ -5,7 +5,7 @@ import os
 import sys
 from discord.ext import commands
 import dobby_cogs
-import dobby_utils 
+from dobby_utils import Cerberus, Gringotts
 
 class Dobby(commands.Bot):
     '''
@@ -13,13 +13,9 @@ class Dobby(commands.Bot):
 
     Keyword arguments for constructor:
     - cmd_prefix -- Command Prefix (default: '.')
-    - base_dir: -- Dobby's root directory (default: os.path.expanduser('~'))
     '''
-    def __init__(self, cmd_prefix:str = '.', base_dir=os.path.expanduser('~')):
+    def __init__(self, cmd_prefix:str = '.'):
         super().__init__(command_prefix=cmd_prefix)
-        
-        # Dobby's parameters
-        self.base_dir = base_dir
         
         # Set up cogs        
         dobby_cogs.set_up_cogs(self)
@@ -43,20 +39,21 @@ class Dobby(commands.Bot):
     # ------------------  USEFUL METHODS  ----------------
 
     def print_configuration(self):
-        print('\n--- Running with following configuration ---')
-        print(f'CMD_PREFIX \'%s\'' % self.command_prefix)
-        print(f'BASE_DIR: \'%s\'' % self.base_dir)
+        print('\n-------  Dobby\'s configuration  --------')
+        print('\n# Dobby:')
+        print(f'Command Prefix: \'%s\'' % self.command_prefix)
+        print('\n# Cerberus:')
+        print(Cerberus.get_configuration())
         
     # ------------------  EVENTS  -----------------------
     async def on_ready(self):
-        print('We have logged in as {0.user}'.format(self)) 
+        print('\nWe have logged in as {0.user}'.format(self)) 
           
     # Processing messages
     async def on_message(self, msg:discord.Message):
         if msg.author == self.user:
             return
         elif msg.content.startswith(self.command_prefix):
-            print(msg.content)
             await self.process_commands(msg)
         elif self.user in msg.mentions:
             return
@@ -72,47 +69,57 @@ class Dobby(commands.Bot):
             txt = '{0.author} has deleted a message...\n Message said:  \n"{0.content}"'.format(msg)
             await msg.channel.send(txt)
 
+# ------------------------------- END OF DOBBY CLASS
 
+# -------------------------------  SETTING UP DOBBY  ----------------------
 def set_up(dobby:commands.Bot):
-    opts, args = getopt.getopt(sys.argv[1:], 'hb:p:', ['help', 'cmd_prefix=', 'base_dir='])
-    for opt, arg in opts:
-        if opt in ('-h', '--help'):  # Help option
-            print_help()
-        elif opt in ('-b', '--base_dir'):
-            if arg is not None and arg != '':
-                if dobby_utils.is_path_traversal(os.path.expanduser('~'), arg):
-                    print('WARNING! Dobby should run inside your home directory...')
-                dobby.base_dir = arg
+    long_options =  ['help', 'cmd-prefix=', 'base-dir=', 'allow-playlist', 'allow-tasks', 'king-role', 'playlist-roles', 'tasks-roles']
+    short_options = 'hb:p:'
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], short_options , long_options)
+        for opt, arg in opts:
+            if opt in ('-h', '--help'):  # HELP option
+                print_help()
+                exit(0)
+            elif opt in ('-b', '--base_dir'):  #  BASE DIR configuration
+                if arg is not None and arg != '':
+                    if dobby_utils.is_path_traversal(os.path.expanduser('~'), arg):
+                        print('WARNING! Dobby should run inside your home directory...')
+                    Cerberus.base_dir = arg
                 
-        elif opt in ('-c', '--cmd_prefix'):
-            if len(arg) == 1:
-                dobby.command_prefix = arg
-            else:
-                print(args, 'is not a valid command prefix!')
-                exit(1)
+            elif opt in ('-p', '--cmd_prefix'):  # CMD PREFIX configuration
+                if len(arg) == 1 and arg in ['.', ':','!','?','*', '/']:
+                    dobby.command_prefix = arg
+                else:
+                    print(args, 'is not a valid command prefix! Max length is 1. Possible prefixes: ' + ','.join(['.', ':','!','?','*', '/'])[:-1])
+                    exit(1)
+
+            elif opt in ('--allow-playlist'):  # Allowing playlist feature
+                Cerberus.allow_playlists = True
+
+            elif opt in ('--allow-tasks'):  # Allowing tasks feature
+                Cerberus.allow_tasks = True
+
+            elif opt in ('--king-role'):  # Establishing role that has full privileges over Dobby
+                pass
             
+            elif opt in ('--playlist-roles'): # Establishing roles for playlist features
+                pass
+
+            elif opt in ('--tasks-roles'): # Establishing roles for tasks features
+                pass
+
+            else:
+                print('Ignoring unknown option \'%s\'' % opt)
+    except getopt.GetoptError as e:
+        print("Unknown option \'%s\'")        
+        exit(1)
+
+
 def print_help():
-    help =  f'######################   D O B B Y   ######################## \
-    \nThis is the help manual for executing dobby discord bot. \
-    \nUsage: {sys.argv[0]}.py [Options]\
-    \n\n Options: \
-    \n\t -h | --help) \
-    \n\t\t Displays this help message.\
-    \n\
-    \n\t -b | --base_dir) \
-    \n\t\t Sets the base directory. Dobby won\'t be allowed to create or read files outside this directory. \
-    \n\
-    \n\t -p | --cmd_prefix) \
-    \n\t\t Sets the command prefix. \
-    \n\
-    \nExample:\
-    \n\t {sys.argv[0]}.py -b ~/discord -c \'!\'\
-    \n\n Note that dobby can be execute with a default configuration. \
-    \n The default configuration is: \
-    \n cmd_prefix = \'.\'\
-    \n base_dir = ~ (dobby executing user\'s home directory) \
-    \n If base_dir has is user home directory, dobby will create/use a subfolder ~/.dobby'
-    print(help)
+    with open('./helptexts/dobby_help.txt') as f:
+        h = "".join(f.readlines())
+    print(h)
 
 # Execute Dobby
 if __name__ == "__main__":
